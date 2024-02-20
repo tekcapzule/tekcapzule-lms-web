8c0ba779b005cfd42c65005a302d897717c79fb3
sathish257216 <sathish.257216@gmail.com>
2024-02-19 22:19:10 -0500

    layout added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   {
@@ -7,24 +8,30 @@ const routes: Routes = [
     loadChildren: () => import('./login/login.module').then((m) => m.LoginModule),
   },
   {
-    path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
-  },
-  {
-    path: 'courses',
-    loadChildren: () => import('./courses/courses.module').then((m) => m.CoursesModule),
-  },
-  {
-    path: 'feeds',
-    loadChildren: () => import('./feeds/feeds.module').then((m) => m.FeedsModule),
-  },
-  {
-    path: 'reports',
-    loadChildren: () => import('./reports/reports.module').then((m) => m.ReportsModule),
-  },
-  {
-    path: 'references',
-    loadChildren: () => import('./references/references.module').then((m) => m.ReferencesModule),
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+      },
+      {
+        path: 'courses',
+        loadChildren: () => import('./courses/courses.module').then((m) => m.CoursesModule),
+      },
+      {
+        path: 'feeds',
+        loadChildren: () => import('./feeds/feeds.module').then((m) => m.FeedsModule),
+      },
+      {
+        path: 'reports',
+        loadChildren: () => import('./reports/reports.module').then((m) => m.ReportsModule),
+      },
+      {
+        path: 'references',
+        loadChildren: () => import('./references/references.module').then((m) => m.ReferencesModule),
+      }
+    ]
   },
   {
     path: '',
@@ -34,7 +41,7 @@ const routes: Routes = [
   {
     path: '**',
     redirectTo: 'dashboard',
-  },
+  }
 ];
 
 @NgModule({
