ba0bc65e91cd75ba547431e3300459a8ed2ed93e
Akhil PB <akhil.pb001@gmail.com>
2024-02-22 01:36:11 +0530

    Added initial signup component

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -2,25 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CallbackComponent } from './callback/callback.component';
 import { LoginComponent } from './login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent,
+    component: LoginComponent
   },
   {
     path: 'callback',
-    component: CallbackComponent,
+    component: CallbackComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
   },
   {
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
-  },
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class LoginRoutingModule {}
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,9 +5,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { CallbackComponent } from './callback/callback.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [LoginComponent, CallbackComponent],
+  declarations: [LoginComponent, CallbackComponent, SignupComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule, LoginRoutingModule]
 })
 export class LoginModule {}
