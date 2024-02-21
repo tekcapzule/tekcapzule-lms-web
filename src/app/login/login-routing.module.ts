d7671d49e9d653a61265d01baaa58a631ccf860c
Akhil PB <akhil.pb001@gmail.com>
2024-02-22 00:59:24 +0530

    Implemented login auth flow for Cognito, Google and Facebook

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CallbackComponent } from './callback/callback.component';
 import { LoginComponent } from './login.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'callback',
+    component: CallbackComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
 ];
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { CallbackComponent } from './callback/callback.component';
 
 @NgModule({
-  declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule],
+  declarations: [LoginComponent, CallbackComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, LoginRoutingModule]
 })
 export class LoginModule {}
