1bc5ada7e9d00ef39cc53ed633640850682e3732
sathish257216 <sathish.257216@gmail.com>
2024-02-27 20:21:08 -0500

    course load

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CourseApiService } from '@app/core';
 import { ICourseDetail } from '@app/shared/models/course-item.model';
 
@@ -9,13 +9,27 @@ import { ICourseDetail } from '@app/shared/models/course-item.model';
   styleUrls: ['./course-detail.component.scss']
 })
 export class CourseDetailComponent implements OnInit {
-  @Input() course: ICourseDetail;
+  course: ICourseDetail;
 
   constructor(
     private router: Router,
-    private courseApi: CourseApiService
+    private courseApi: CourseApiService,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      //this.pageId = params['pageId'];
+      this.getWishlistCourse(params['code']);
+    });
+  }
+
+  getWishlistCourse(code: string) {
+    this.courseApi.getWishlistCourse().subscribe(
+      data => {
+        this.course = data.find(c => c.learningMaterialId === code) as ICourseDetail;
+      },
+      err => {}
+    );
   }
 }
