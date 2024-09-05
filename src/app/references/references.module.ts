6e5e9decbdee23551f61368d661a461a6e228eb6
sathish257216 <sathish.257216@gmail.com>
2024-09-04 23:44:32 -0400

    Added filterby lists in reference module

--- src/app/references/references.module.ts
+++ src/app/references/references.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ReferencesRoutingModule } from './references-routing.module';
 import { ReferencesComponent } from './references.component';
+import { SharedModule } from '@app/shared/shared.module';
 
 @NgModule({
   declarations: [ReferencesComponent],
-  imports: [CommonModule, ReferencesRoutingModule],
+  imports: [CommonModule, ReferencesRoutingModule, SharedModule],
 })
 export class ReferencesModule {}
