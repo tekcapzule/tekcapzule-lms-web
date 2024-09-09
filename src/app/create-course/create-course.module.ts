47313e89a795095928369ab94dbd5996c4c24e95
Sasikumar P <sasikumarp36@gmail.com>
2024-09-09 18:47:06 +0530

    create course ui changes

--- src/app/create-course/create-course.module.ts
+++ src/app/create-course/create-course.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CreateCourseComponent } from './create-course.component';
 import { CreateCourseRoutingModule } from './create-course-routing.module';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputTextModule } from 'primeng/inputtext';
+import { FileUploadModule } from 'primeng/fileupload';
+import { CalendarModule } from 'primeng/calendar';
+import { SidebarModule } from 'primeng/sidebar';
 
 @NgModule({
   declarations: [CreateCourseComponent],
-  imports: [CommonModule, CreateCourseRoutingModule],
+  imports: [CommonModule, CreateCourseRoutingModule, DropdownModule, FormsModule, RadioButtonModule, InputTextModule, FileUploadModule, ReactiveFormsModule, CalendarModule, SidebarModule],
 })
-export class CreateCourseModule {}
+export class CreateCourseModule { }
